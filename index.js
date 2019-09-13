1adb4bd83670703a0c1fc873f6527190b3402cc9
xxultragunnerxx <49918799+xxultragunnerxx@users.noreply.github.com>
2019-09-12 21:27:59 -0400

    create file indext
    Importing from my repl.i

---
+++ index.js
@@ -0,0 +1,45 @@
+const apiKey = "";
+
+function formatQueryParams(userObject) {
+  const items = Object.keys(userObject).map(key => `${key}=${userObject[key]}`)
+  return items.join("&");
+}
+
+function main() {
+  $(`fieldset`).on("click", "#submit", function () {
+    event.preventDefault();
+    let userQuery = $('#text-box').val();
+    getUserName(userQuery);
+  });
+}
+
+function getUserName(username) {
+  const searchUrl = `https://api.github.com/users/${username}/repos`;
+
+  fetch(searchUrl)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+
+}
+
+const displayResults = (responseJson) => {
+    $('.search-results').empty();
+  responseJson.forEach((repoObj) => {
+    $('.search-results').append(`
+          <div>${repoObj.name}</div>  
+          <div>${repoObj.html_url}</div>
+       `)
+  })
+
+}
+
+
+$(main);
